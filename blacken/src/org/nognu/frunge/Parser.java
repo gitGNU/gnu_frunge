7e650957e216d58477973c272ab8ba5e431e41c9
Dennis Heidsiek <HeidsiekB@aol.com>
2009-09-30 16:51:44 +0200

    * ADD: (blacken) about message, files changed to linux line style

--- blacken/src/org/nognu/frunge/Parser.java
+++ blacken/src/org/nognu/frunge/Parser.java
@@ -1,11 +1,23 @@
 package org.nognu.frunge;
 
 
+import java.io.PrintStream;
+
+
 public class Parser {
-
-
+   
+  protected static void printAbout(PrintStream f) {
+    f.format("Blacken (s to ſ converter)%n");
+    f.format("This programm is free software (GNU General Public License 3 or later)%n");
+    f.format("Links:%n");
+    f.format("licence text : http://www.gnu.org/licenses/%n");
+    f.format("source code  : http://git.savannah.gnu.org/cgit/frunge.git/tree/blacken%n");
+    f.format("project site : http://sv.nongnu.org/p/frunge%n");     
+    f.format("email contact: frunge-external@nongnu.org%n");
+  }
+  
   public static void main(String ... arg) {
-      System.out.format("Hello word!%n");
+      printAbout(System.out);
       
       System.out.format("Arguments:%n");
       for(int i=0; i<arg.length; i++) {
