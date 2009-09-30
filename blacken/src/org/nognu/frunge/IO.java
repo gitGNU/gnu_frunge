4e162591250ddd8122a5a7369518011de698557f
Dennis Heidsiek <HeidsiekB@aol.com>
2009-09-30 18:07:02 +0200

    ADD: (blacken) support for utf-8 encoded text files

--- blacken/src/org/nognu/frunge/IO.java
+++ blacken/src/org/nognu/frunge/IO.java
@@ -1,6 +1,9 @@
 package org.nognu.frunge;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,21 +14,38 @@ import java.util.List;
  */
 public class IO {
 	
-	protected static List<String> PREFIXES = Arrays.asList("", "bin/", "out/");
+	protected final static List<String> PREFIXES = Arrays.asList("", "bin/", "out/");
+	
+	protected final static ClassLoader SCL = ClassLoader.getSystemClassLoader();
 
-	protected static InputStream openStream(String s) {
-		return ClassLoader.getSystemClassLoader().getResourceAsStream(s);
-	}
 
+	/**
+	 * @param s a relative link to a resource
+	 * @return null in case of an error
+	 */
 	public static InputStream getStream(String s) {
 		InputStream is = null;
 		for(String p : PREFIXES) {
-			is = openStream(p.concat(s));
+			is = SCL.getResourceAsStream(p.concat(s));
 			if(is != null) {
 				break;
 			}
 		}
 		return is;
+	}
+
+	/**
+	 * @param s a relative link to a UTF-8 encoded text file
+	 * @return null in case of an error 
+	 */
+	public static BufferedReader getReader(String s) {
+		BufferedReader r = null;
+		try {
+			r = new BufferedReader(new InputStreamReader(IO.getStream(s), "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return r;
 	}
 	
 }
