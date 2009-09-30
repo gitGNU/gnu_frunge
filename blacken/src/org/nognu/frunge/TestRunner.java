560dab4e51acd4fed3d66b3fdbf4e880eaaa56d9
Dennis Heidsiek <HeidsiekB@aol.com>
2009-09-30 22:06:59 +0200

    ADD: (blacken) i18n of the tests

--- blacken/src/org/nognu/frunge/TestRunner.java
+++ blacken/src/org/nognu/frunge/TestRunner.java
@@ -1,6 +1,8 @@
 package org.nognu.frunge;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -10,22 +12,28 @@ import java.io.*;
  */
 public class TestRunner {
 
+	protected final static List<String> LANGUAGES = Arrays.asList("de", "en");
+	
 	public TestRunner() {
-		System.out.format("Run tests:%n");
-		
-		try {
-			BufferedReader r = IO.getReader("testcases/de.csv");
+		for(String lang : LANGUAGES) {
+			System.out.format("Run tests for %s:%n", lang);
+			
+			try {
+				BufferedReader r = IO.getReader("testcases/"+lang+".csv");
 
-			String line;
-			while ((line = r.readLine()) != null) {
-				int pos = line.indexOf(";");
-				String in = line.substring(0, pos);
-				String out = line.substring(pos + 1, line.length());
+				String line;
+				while ((line = r.readLine()) != null) {
+					int pos = line.indexOf(";");
+					String in = line.substring(0, pos);
+					String out = line.substring(pos + 1, line.length());
 
-				System.out.format("Testcase: %s -> %s%n", in, out);
+					System.out.format("Testcase: %s -> %s%n", in, out);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			
+			System.out.format("%n");
 		}
 	}
 
