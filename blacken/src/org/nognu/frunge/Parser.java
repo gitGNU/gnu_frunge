3a0fdc62c6024afbbc50ab24dd4b0d1016892730
Dennis Heidsiek <HeidsiekB@aol.com>
2009-10-01 07:55:05 +0200

    FIX: (blacken) minor

--- blacken/src/org/nognu/frunge/Parser.java
+++ blacken/src/org/nognu/frunge/Parser.java
@@ -28,9 +28,7 @@ public class Parser {
 			System.out.format("arg[%d]=%s%n", i, arg[i]);
 		}
 		System.out.format("%n");
-
-		if ((arg.length==0) || (arg[0].equals("-test"))) {
-			new TestRunner();
-		}
+		
+		new TestRunner();
 	}
 }
