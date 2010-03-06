9dc8c5d321a19f5ce6c0a3f5db1fb030c90a7db2
Dennis Heidsiek <HeidsiekB@aol.com>
2010-03-06 15:56:09 +0100

    ADD: (blacken) unclear situation: ß in latin

--- blacken/src/org/nongnu/frunge/converter/LaConverter.java
+++ blacken/src/org/nongnu/frunge/converter/LaConverter.java
@@ -2,6 +2,11 @@ package org.nongnu.frunge.converter;
 
 /**
  * A very simple {@link Converter} for the Latin language.
+ * 
+ * <p>Still to reſolve:
+ * <a href="http://de.wikipedia.org/wiki/Diskussion:%C3%9F#.C3.9F_in_Lateinischer_Sprache.3F">ß in Latin</a>,
+ * <a href="http://de.wikipedia.org/wiki/Datei:Trithemius-Polygraphiae-71.jpg">related image</a>.</p>
+ * 
  */
 public class LaConverter implements Converter {
 	
