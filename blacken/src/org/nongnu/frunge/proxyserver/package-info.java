44dfd8f71f4ac721a5763d4dbae5a3182bae8505
Dennis Heidsiek <HeidsiekB@aol.com>
2010-03-07 21:52:14 +0100

    ADD: (blacken) minor

--- blacken/src/org/nongnu/frunge/proxyserver/package-info.java
+++ blacken/src/org/nongnu/frunge/proxyserver/package-info.java
@@ -1,4 +1,6 @@
 /**
  * <p>A proxy server interface to blacken html files from the internet in every browser.</p>
+ * 
+ * <p>Inspired by the various services at <a href="http://www.parallelnetz.de/">parallelnetz.de/</a>.</p>
  */
 package org.nongnu.frunge.proxyserver;
